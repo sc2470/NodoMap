229985c639f7550257b93d39e5ff97142242eccb
sc2470 <150459789+sc2470@users.noreply.github.com>
2025-12-21 17:27:52 +0000

    Update FindMarkers.R

--- Markers/FindMarkers.R
+++ Markers/FindMarkers.R
@@ -2,9 +2,9 @@
 library(Seurat)
 library(dplyr)
 
-data <- readRDS("/home/rstudio/2023_April_Integration/nodose_integrated_PC30-60_res0.6-2/nodose-integrated-pcs-50-resolution-1.4.RDS")
+data <- readRDS("nodomap_cleaned.RDS")
 DefaultAssay(data) = "originalexp"
-data <- SetIdent(data, value = "integrated_snn_res.1.4")
+data <- SetIdent(data, value = "seurat_clusters")
 
 #Find markers of every clusters, only show positive ones
 data.marker <- FindAllMarkers(data, only.pos = T, min.pct = 0.25)
@@ -17,14 +17,14 @@ genes_to_ens<-function(gene_symbol)+
 
 data.marker$genesymbol <- rownames(mouse_gene_names[match(data.marker$gene, mouse_gene_names$Gene.stable.ID),])
 
-write.table(data.marker, file = "pc50-resolution-1.4-markers-230613.txt", sep = "\t", quote = F, col.names = NA)
+write.table(data.marker, file = "251019_nodose_markers.txt", sep = "\t", quote = F, col.names = NA)
 
 data.marker %>%
   group_by(cluster) %>%
   slice_max(n = 3, order_by = avg_log2FC)
 
 #Label clusters with marker genes
-markers <- read.delim("/home/rstudio/2023_April_Integration/pc50-resolution-1.4-markers-230613.txt")
+markers <- read.delim("251019_nodose_markers.txt")
 
 markers$prop <- markers$avg_log2FC*(markers$pct.1 + 0.01)/(markers$pct.2 + 0.01)
 
